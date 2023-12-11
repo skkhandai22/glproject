de14101ed7bd52fb3c6ffab63e66924dd26a294a
skkhandai22 <54287652+skkhandai22@users.noreply.github.com>
2023-12-11 11:04:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-class Cosmos:
-    URL = "https://glproject.documents.azure.com:443/"
-    KEY = "mxJFS75xj2V4GMg0QiGCTzKZvQYmSWJIWEq6mZWWfKwaKHX7VsMibuFjyLSFSuN0WBbQBKloqY1oACDbHHxdAw=="
-    DATABASE_NAME = 'glprojectdatabase'
-    CONTAINER_NAME = 'userdetails'
+
